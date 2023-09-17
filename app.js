83bdbdafd43642dfdd57e9f14506a39645b49349
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2023-09-17 08:15:59 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -54,27 +54,108 @@
 // sri=>batting
 // ind=>win
 
-var rain_per = +prompt("Enter Rain Per")
-// 
-if (rain_per >= 80) {
-    console.log("Match Draw")
+// var rain_per = +prompt("Enter Rain Per")
+// // 
+// if (rain_per >= 80) {
+//     console.log("Match Draw")
+// }
+// else {
+//     console.log("enter toss win team")
+//     var toss_win = prompt("Enter Toss Win")
+//     if (toss_win == "ind") {
+//         console.log("win srilanka")
+//     }
+//     else if (toss_win == "sri") {
+//         console.log("win ind")
+
+//     }
+//     else {
+//         console.log(toss_win + "bhai ya team ha hei nhi")
+//     }
+// }
+
+// var a = ["1","1"]
+
+// var food_name="PIZZA"
+// alert(food_name+"\n"+food_name.slice(0,4)+"\n"+food_name.slice(0,3)+"\n"+food_name.slice(0,2)+"\n"+food_name.slice(0,1))
+
+
+// var email = "abc@gmail.com"
+// // alert("my email is "+ email)
+// document.write("<h1 style='color:red'>mty email is "+email+"</h1>")
+
+// var test = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(test)
+
+// var customerName = prompt("Enter Your Name")
+// var productName = prompt("Enter Product NAME")
+
+// if(productName.toLowerCase()== "mike" || productName.toUpperCase() == "keyboard" || productName.toLowerCase()=="mouse"){
+//     var price= 200
+//     var quantity =  +prompt("Enter Product Quantity")
+//     var res = price*quantity
+//     document.write(customerName +" Order :"+ productName + " quantity : "+ quantity+ " total : " + res )
+// }
+// else if(productName.toLowerCase()== "laptop" ){
+//     var price= 20000
+//     var quantity =  +prompt("Enter Product Quantity")
+//     var res = price*quantity
+//     document.write(customerName +" Order :"+ productName + " quantity : "+ quantity+ " total : " + res )
+// }
+
+// else if(productName.toLowerCase()== "speaker" || productName.toUpperCase() == "usb" ){
+//     var price= 1000
+//     var quantity =  +prompt("Enter Product Quantity")
+//     var res = price*quantity
+//     document.write(customerName +" Order :"+ productName + " quantity : "+ quantity+ " total : " + res )
+// }
+// else{
+//     document.write(productName+" not aviable in my store")
+// }
+
+//chapter no = 4
+//task 1
+// var name,class_name,age;
+// name="asad";
+
+// task 2
+// lellgal variable
+// var a=10
+// var a1=12
+// var 
+
+//
+// 1var=0
+// -var1=0;
+// name 2=9;
+// var nar+3;
+// var class
+
+// document.write("A heading stating “Rules for naming JS variables”")
+
+// document.write("Variable names can only contain ______, ______,
+// ______ and ______.")
+
+var numb1 = +prompt("Enter Number 1")
+var numb2 = +prompt("Enter Number 2")
+var numb3 = +prompt("Enter Number 3")
+
+// numb1=>3,5,5
+if(numb1>numb2 && numb1>numb3){
+    document.write("Number 1 is greater ")
 }
-else {
-    console.log("enter toss win team")
-    var toss_win = prompt("Enter Toss Win")
-    if (toss_win == "ind") {
-        console.log("win srilanka")
-    }
-    else if (toss_win == "sri") {
-        console.log("win ind")
-
-    }
-    else {
-        console.log(toss_win + "bhai ya team ha hei nhi")
-    }
+else if(numb2>numb1 && numb2>numb3){
+    document.write("Number 2 is greater ")
 }
-
-
+else if(numb3>numb1 && numb3>numb2){
+    document.write("Number 3 is greater ")
+}
+else if(numb1==numb2 && numb1 > numb3){
+    document.write(" greater and equal 1 and 2")
+}
+else if(numb2==numb3 && numb2 > numb1){
+    document.write(" greater and equal 2 and 3")
+}
 
 
 
