a0ac5353cccba4102e7ba432b337ad7ce7228400
MuhammadZubair786 <mzhassan444@gmail.com>
2023-10-22 07:46:00 -0700

    add files

---
+++ app.js
@@ -0,0 +1,71 @@
+
+// // var userName   ="javascRipt         devEloper.            learn               jaVascRipt.          testIng                app"
+// // userName = userName.trim()
+// // userName = userName[0].toUpperCase()+userName.substring(1).toLowerCase()
+// // var startloop=0;
+// // // space remove
+// // // userName = userName.replace(/\s+/g, " ");
+// // userName = userName.replace(/a/g,"--")
+
+
+// // for(var i=0;i<userName.length;i++){
+// //     if(userName.charCodeAt(i)==46 ){
+// //         if(userName.charCodeAt(i+1)==32){
+// //         let previousdata = userName.substring(0,i+1)
+// //         let newdata = userName.substring(i+2)
+// //         userName = previousdata+newdata
+// //         }
+// //     }
+// // }
+// // console.log(userName)
+// // console.log(userName)
+// // i=startloop
+// // while(i<userName.length){
+// //     if(userName.charCodeAt(i)==46){
+// //         console.log(i)
+// //         let previousdata = userName.substring(0,i+1) //previous data 
+// //         let newdata = userName[i+1].toUpperCase()+ userName.substring(i+2).toLowerCase()
+// //         userName = previousdata + newdata
+// //         console.log(userName)
+// //     }
+// //     i++
+// // }
+
+// // var a = (Math.random()*10)
+// // var b = parseInt(a)
+// // var c = parseFloat(a)
+// // console.log(a,b,c)
+// // 0 or 1
+
+// // date And Time
+// var date = new Date()
+
+// var month = ["January","February","March","April","May","June","July",
+// "August","September","October","November","December"];
+
+
+// var day = ["sun","mon","tues","wed",""]
+// // console.log(date.getDay())
+// console.log(date.toString().substring(0,3)+"day")
+// console.log(date.toString().substring(4,7))
+// // console.log(date.getFullYear())
+// console.log(month[date.getMonth()])
+
+
+// // switch(date.getDay()){
+// //     case 0 :
+// //         console.log("Sunday")
+// //         break;
+// //     case 1:
+// //         console.log("Monday")
+// //         break;
+// //     default:
+// //         console.log("Tuesday")
+// //         break;
+
+// // }
+
+// // atm => abstract 
+// // account no 
+
+
